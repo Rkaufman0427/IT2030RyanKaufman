38fe190d4338c1eef89e56fb62d35c5b723cd27a
Rkaufman0427 <70352842+Rkaufman0427@users.noreply.github.com>
2022-10-06 22:27:38 -0400

    Delete Week3lab directory

--- Week3lab/Week3-SampleCode/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AddMulTwoNum.Models
-{
-   public class ErrorViewModel
-   {
-      public string? RequestId { get; set; }
-
-      public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-   }
-}
