5fb54ec859ef0adcf3efe6d61acd0f3e2b8f75d1
Rkaufman0427 <wahootribe24@gmail.com>
2022-10-12 19:00:37 -0400

    Commit

---
+++ Week_5_Lab/Week_5_Lab/Week_5_Lab/Models/TaxModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Week_3_Lab.Models
+{
+    public class TaxModel
+    {
+        [Required(ErrorMessage = "Please enter a Subtotal.")]
+        public double? Subtotal { get; set; }
+
+        [Required(ErrorMessage = "Please enter a tax percent.")]
+        public double? TaxPercent { get; set; }
+
+        public double? CalculateTaxAmount()
+        {
+            double? TaxAmountCal = TaxPercent * 0.01;
+            double? TaxAmount = TaxAmountCal * Subtotal;
+          
+            return TaxAmount;
+        }
+
+        public double? CalculateTotal()
+        {
+            double? Total = Subtotal + CalculateTaxAmount();
+
+            return Total;
+        }
+
+    }
+}
