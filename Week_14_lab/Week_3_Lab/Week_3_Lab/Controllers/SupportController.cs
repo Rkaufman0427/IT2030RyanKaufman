e4145b3b910811013f438f164e6551d5154aa384
Rkaufman0427 <wahootribe24@gmail.com>
2022-12-12 22:47:18 -0500

    commit

---
+++ Week_14_lab/Week_3_Lab/Week_3_Lab/Controllers/SupportController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Week_3_Lab.Models;
+
+namespace Week_3_Lab.Controllers
+{
+    public class SupportController : Controller
+    {
+        [HttpGet]
+        public IActionResult Support()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Support(Customer customer)
+        {
+            var Customer = new Customer();
+            return View(customer);
+        }
+    }
+}
