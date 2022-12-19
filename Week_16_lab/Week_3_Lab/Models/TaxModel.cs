8693e5bc49b1e51f0694c831fbfa5d8ecb2658da
Rkaufman0427 <wahootribe24@gmail.com>
2022-12-18 19:04:27 -0500

    Commit

---
+++ Week_16_lab/Week_3_Lab/Models/TaxModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Week_3_Lab.Models
+{
+    public class TaxModel
+    {
+        [Required(ErrorMessage = "Please enter a Subtotal.")]
+        [Range(0, 9999999, ErrorMessage = "Enter a value between 0 and 9999999.")]
+        public double? Subtotal { get; set; }
+
+        [Required(ErrorMessage = "Please enter a Tax Percent.")]
+        [Range(0, 100, ErrorMessage = "Enter a value between 0 and 100.")]
+        public double? TaxPercent { get; set; }
+
+        public double? TaxAmount { get; set; }
+        public double? Total { get; set; }
+
+
+        public double? CalculateTaxAmount()
+        {
+            double? TaxAmountCal = TaxPercent * 0.01;
+            double? TaxAmount = TaxAmountCal * Subtotal;
+          
+            return TaxAmount;
+        }
+
+        public double? CalculateTotal()
+        {
+            double? Total = Subtotal + CalculateTaxAmount();
+
+            return Total;
+        }
+
+    }
+}
